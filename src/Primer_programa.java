e5767984a83629036eac9a9dbb9b5bd49bbce0a3
Miguel González <gonzalezfelez25@iespabloserrano.com>
2025-11-20 10:06:15 +0100

    Primera versión del programa Primer_programa.java

---
+++ src/Primer_programa.java
@@ -0,0 +1,19 @@
+
+public class Primer_programa {
+
+	public static void main(String[] args) {
+		
+		int variable=5;
+		variable++;
+		variable = variable + 10;
+		System.out.println("La variable es: "+ variable);
+		
+		int contador=0;
+		
+		while(contador<=10) {
+			System.out.println("El numero es: " + contador);
+			contador++;
+		}
+	}
+
+}
